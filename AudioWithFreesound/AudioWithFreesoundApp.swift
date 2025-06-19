55666274956be5e2c06139ac3781a9f97b9d2b44
Ibrahima Gueye <ibkhgueye@gmail.com>
2025-06-19 12:52:33 +0200

    Initial Commit

---
+++ AudioWithFreesound/AudioWithFreesoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioWithFreesoundApp.swift
+//  AudioWithFreesound
+//
+//  Created by Ibrahima KH GUEYE on 19/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct AudioWithFreesoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
